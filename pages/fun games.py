8865cc00907a49f3cbd49ee31952036483052874
rithik102938 <rithikveswakumarkesana@gmail.com>
2024-10-11 19:19:14 -0700

    soccer

--- pages/fun games.py
+++ pages/fun games.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.title("Lets play addition game")
+st.title("Lets play Soccer")
 # HTML for the Scratch iframe
 scratch_iframe = '''
-<iframe src="https://scratch.mit.edu/projects/760756130/embed" 
+<iframe src="https://scratch.mit.edu/projects/621172683/embed" 
         allowtransparency="true" 
         width="485" 
         height="402" 
         frameborder="0" 
-        scrolling="no" 
+        scrolling="no" 621172683
         allowfullscreen></iframe>
 '''
 
