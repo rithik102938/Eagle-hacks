b53c98f53d0d881683b643675b32a81b5c3359ad
rithik102938 <rithikveswakumarkesana@gmail.com>
2024-10-12 08:47:50 -0700

    games

--- pages/fun games.py
+++ pages/fun games.py
@@ -18,34 +18,3 @@ scratch_iframe = '''
 
 # Display the iframe in the Streamlit app
 st.components.v1.html(scratch_iframe, height=402)
-
-
-st.header("Soccer")
-# HTML for the Scratch iframe
-scratch_iframe = '''
-<iframe src="https://scratch.mit.edu/projects/621172683/embed" 
-        allowtransparency="true" 
-        width="485" 
-        height="402" 
-        frameborder="0" 
-        scrolling="no" 621172683
-        allowfullscreen></iframe>
-'''
-
-# Display the iframe in the Streamlit app
-st.components.v1.html(scratch_iframe, height=402)
-
-st.header("Glide Bird")
-# HTML for the Scratch iframe
-scratch_iframe = '''
-<iframe src="https://scratch.mit.edu/projects//embed" 
-        allowtransparency="true" 
-        width="485" 
-        height="402" 
-        frameborder="0" 
-        scrolling="no" 621172683
-        allowfullscreen></iframe>
-''' 
-
-# Display the iframe in the Streamlit app
-st.components.v1.html(scratch_iframe, height=402)
